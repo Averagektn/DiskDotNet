bb701aa59155fdde16b7b4c8d7f2b0874660fb94
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-01-14 17:03:27 +0300

    Working on connection

--- Disk/Data/Impl/Connection.cs
+++ Disk/Data/Impl/Connection.cs
@@ -1,12 +1,33 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Net;
 
 namespace Disk.Data
 {
     class Connection : IDataSource
     {
-        public Connection()
+        private static List<Connection> connnections = [];
+        private IPAddress IP;
+        private int Port;
+        private Connection(IPAddress ip, int port)
         {
+            IP = ip;
+            Port = port;
+            // Establish connection
+        }
 
+        public static Connection GetConnection(IPAddress ip, int port)
+        {
+            var conn = connnections.FirstOrDefault(c => c.IP.Equals(ip) && c.Port == port);
+
+            if (conn is null)
+            {
+                var c = new Connection(ip, port);   
+                connnections.Add(c);
+                return c;
+            }
+
+            return conn;
         }
 
         public Point GetXY()
