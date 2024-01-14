b5c89451404144f00315707eb6cd1397ae44c592
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-01-14 23:41:03 +0300

    Shot receiving

--- Disk/Visual/Impl/Target.cs
+++ Disk/Visual/Impl/Target.cs
@@ -5,9 +5,11 @@ namespace Disk.Visual
 {
     class Target(Point center, int radius, int speed, Brush color, Point iniSize) : Circle(center, radius, speed, color, iniSize)
     {
-/*        public void ReceiveShot(Point shot)
+        public bool Contains(Point shot)
         {
-            if ()
-        }*/
+            double distance = Math.Sqrt(Math.Pow((shot.X - Center.X) / Radius, 2) + Math.Pow((shot.Y - Center.Y) / Radius, 2));
+
+            return distance <= 1.0f;
+        }
     }
 }
