e94b53c1956c914259dc754def093e56cab6f5aa
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-01-20 14:13:05 +0300

    Enemy following alorithm implemented

--- Disk/Visual/Impl/Enemy.cs
+++ Disk/Visual/Impl/Enemy.cs
@@ -12,6 +12,35 @@ namespace Disk.Visual.Impl
             base(center, radius, speed, color, iniSize)
         { }
 
-        public void Follow() { }
+        public void Follow(Point2D<int> target) 
+        {
+            var direction = GetAngleDirection(target);
+
+            bool moveTop = IsBetween(direction, 315, 45);
+            bool moveBottom = IsBetween(direction, 45, 135);
+            bool moveRight = IsBetween(direction, 135, 225);
+            bool moveLeft = IsBetween(direction, 225, 315);
+
+            Move(moveTop, moveRight, moveBottom, moveLeft);
+        }
+
+        private float GetAngleDirection(Point2D<int> target)
+        {
+            var deltaX = target.X - Center.X;
+            var deltaY = target.Y - Center.Y;
+
+            var angleRadians = Math.Atan2(deltaY, deltaX);
+
+            var angleDegrees = angleRadians * (180 / Math.PI);
+
+            if (angleDegrees < 0)
+            {
+                angleDegrees += 360;
+            }
+
+            return (float)angleDegrees;
+        }
+
+        private static bool IsBetween(float angle, float left, float right) => angle >= left && angle <= right;
     }
 }
