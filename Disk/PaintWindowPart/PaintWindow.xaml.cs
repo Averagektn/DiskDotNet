487693d02c845c92800d487b80597952ec446dca
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-02-18 16:02:58 +0300

    PaintWindow separated

--- Disk/PaintWindowPart/PaintWindow.xaml.cs
+++ Disk/PaintWindowPart/PaintWindow.xaml.cs
@@ -1,22 +1,17 @@
-﻿using Disk.Calculations.Impl;
-using Disk.Calculations.Impl.Converters;
+﻿using Disk.Calculations.Impl.Converters;
 using Disk.Data.Impl;
 using Disk.Visual.Impl;
 using Disk.Visual.Interface;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Timers;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using FilePath = System.IO.Path;
-using Path = Disk.Visual.Impl.Path;
 using Point2DF = Disk.Data.Impl.Point2D<float>;
 using Point2DI = Disk.Data.Impl.Point2D<int>;
 using Point3DF = Disk.Data.Impl.Point3D<float>;
-using PolarPointF = Disk.Data.Impl.PolarPoint<float>;
 using Settings = Disk.Config.Config;
 using Timer = System.Timers.Timer;
 
@@ -108,11 +103,11 @@ namespace Disk
 
         private Size DataPanelSize => DataRect.RenderSize;
 
-        private string MovingToTargetLogName => 
+        private string MovingToTargetLogName =>
             $"{CurrPath}{FilePath.DirectorySeparatorChar}Движение к мишени {TargetID}.log";
-        private string OnTargetLogName => 
+        private string OnTargetLogName =>
             $"{CurrPath}{FilePath.DirectorySeparatorChar}В мишени {TargetID - 1}.log";
-        private string TargetReachedLogName => 
+        private string TargetReachedLogName =>
             $"{CurrPath}{FilePath.DirectorySeparatorChar}Мишень {TargetID - 1} поражена.log";
 
         private string UsrWndLog => $"{CurrPath}{FilePath.DirectorySeparatorChar}{Settings.USER_WND_LOG_FILE}";
@@ -242,8 +237,6 @@ namespace Disk
             Application.Current.Dispatcher.Invoke(() => TblScore.Text = $"Счет: {Score}");
         }
 
-        private void OnClosing(object? sender, CancelEventArgs e) => StopGame();
-
         private void MoveTimerElapsed(object? sender, ElapsedEventArgs e)
             => Application.Current.Dispatcher.Invoke(() => User?.Move(ShiftedWndPos ?? User.Center));
 
@@ -265,59 +258,6 @@ namespace Disk
                         }*/
         }
 
-        private void ShowStats()
-        {
-            using var userAngleReader = FileReader<float>.Open(UsrAngLog, Settings.LOG_SEPARATOR);
-
-            var dataset = userAngleReader.Get2DPoints().ToList();
-
-            if (dataset.Count != 0)
-            {
-                var mx = Calculator2D.MathExp(dataset);
-                var dispersion = Calculator2D.Dispersion(dataset);
-                var deviation = Calculator2D.StandartDeviation(dataset);
-
-                MessageBox.Show(
-                $"""
-                Счет: {Score}
-                Среднее смещение от центра: {mx}
-                Дисперсия: {dispersion}
-                Среднее отклонение от центра: {deviation}
-                """);
-            }
-            else
-            {
-                MessageBox.Show("Конец");
-            }
-        }
-
-        private void StopGame()
-        {
-            if (Target is not null)
-            {
-                Target.Remove(PaintArea.Children);
-                User?.Remove(PaintArea.Children);
-
-                Drawables.Remove(Target);
-                Scalables.Remove(Target);
-
-                Drawables.Remove(User);
-                Scalables.Remove(User);
-            }
-
-            IsGame = false;
-
-            NetworkThread.Join();
-
-            MoveTimer.Stop();
-            ShotTimer.Stop();
-
-            UserLogAng?.Dispose();
-            UserLogWnd?.Dispose();
-            UserLogCen?.Dispose();
-            UserMovementLog?.Dispose();
-        }
-
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
             Converter = new(SCREEN_INI_SIZE, new(X_ANGLE_SIZE, Y_ANGLE_SIZE));
@@ -336,7 +276,7 @@ namespace Disk
             else
             {
                 MapReader = FileReader<float>.Open(MapFilePath);
-                Target = new(Converter.ToWnd_FromRelative(MapReader.GetXY() ?? new(0.5f, 0.5f)), 
+                Target = new(Converter.ToWnd_FromRelative(MapReader.GetXY() ?? new(0.5f, 0.5f)),
                     Settings.TARGET_INI_RADIUS, SCREEN_INI_SIZE, TargetHP);
             }
 
@@ -378,109 +318,6 @@ namespace Disk
 
             MoveTimer.Start();
             ShotTimer.Start();
-        }
-
-        private void OnSizeChanged(object sender, RoutedEventArgs e)
-        {
-            AllowedArea.RadiusX = PaintPanelCenterX;
-            AllowedArea.RadiusY = PaintPanelCenterY;
-            AllowedArea.Center = new(PaintPanelCenterX, PaintPanelCenterY);
-
-            foreach (var elem in Scalables)
-            {
-                elem?.Scale(PaintPanelSize);
-            }
-        }
-
-        private void CbTargets_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            PaintArea.Children.Clear();
-            foreach (var elem in Drawables)
-            {
-                elem?.Draw(PaintArea);
-            }
-
-            var selectedIndex = CbTargets.SelectedIndex;
-            var roseFileName = $"{CurrPath}{FilePath.DirectorySeparatorChar}В мишени {selectedIndex + 1}.log";
-            var pathFileName = $"{CurrPath}{FilePath.DirectorySeparatorChar}Движение к мишени {selectedIndex + 1}.log";
-
-            if (selectedIndex != -1 && Converter is not null && Target is not null)
-            {
-                if (RbRose.IsChecked ?? false)
-                {
-                    if (File.Exists(roseFileName))
-                    {
-                        using var userReader = FileReader<float>.Open(roseFileName, Settings.LOG_SEPARATOR);
-
-                        var angRadius = Converter.ToAngleX_FromLog(Target.Radius) +
-                            Converter.ToAngleY_FromLog(Target.Radius) / 2;
-
-                        var a = userReader.Get2DPoints().ToList();
-                        var dataset =
-                            a
-                            .Select(p => new PolarPointF(p.X - TargetCenters[selectedIndex].X, p.Y -
-                            TargetCenters[selectedIndex].Y, null))
-                            .Where(p => Math.Abs(p.X) > angRadius && Math.Abs(p.Y) > angRadius).ToList();
-
-                        var userRose = new Graph(dataset, PaintPanelSize, Brushes.LightGreen, 8);
-
-                        userRose.Draw(PaintArea);
-
-                        Scalables.Add(userRose);
-                    }
-                }
-                else if (RbPath.IsChecked ?? false)
-                {
-                    if (File.Exists(pathFileName))
-                    {
-                        using var userPathReader = FileReader<float>.Open(pathFileName, Settings.LOG_SEPARATOR);
-
-                        var userPath = new Path(userPathReader.Get2DPoints(), PaintPanelSize, new(X_ANGLE_SIZE, Y_ANGLE_SIZE),
-                            new SolidColorBrush(Color.FromRgb(Settings.USER_COLOR.R, Settings.USER_COLOR.G,
-                            Settings.USER_COLOR.B)));
-
-                        userPath.Draw(PaintArea);
-
-                        Scalables.Add(userPath);
-                    }
-                }
-            }
-        }
-
-        private void RbRose_Checked(object sender, RoutedEventArgs e)
-        {
-            CbTargets.Items.Clear();
-
-            for (int i = 1; i < TargetID; i++)
-            {
-                CbTargets.Items.Add($"Роза ветров для цели {i}");
-            }
-        }
-
-        private void RbPath_Checked(object sender, RoutedEventArgs e)
-        {
-            CbTargets.Items.Clear();
-
-            for (int i = 1; i < TargetID; i++)
-            {
-                CbTargets.Items.Add($"Путь к цели {i}");
-            }
-        }
-
-        private void OnStopClick(object sender, RoutedEventArgs e)
-        {
-            StopGame();
-            ShowStats();
-
-            BtnStop.IsEnabled = false;
-
-            for (int i = 1; i < TargetID; i++)
-            {
-                CbTargets.Items.Add($"Роза ветров для цели {i}");
-            }
-            CbTargets.Visibility = Visibility.Visible;
-            RbPath.Visibility = Visibility.Visible;
-            RbRose.Visibility = Visibility.Visible;
         }
     }
 }
