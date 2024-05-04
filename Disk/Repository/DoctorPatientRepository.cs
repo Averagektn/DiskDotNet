50825702a7acb005a36f55d7e30f2569de90be10
AverageKtn <rylonoboev2004@gmail.com>
2024-05-04 14:03:17 +0300

    created repositories

---
+++ Disk/Repository/DoctorPatientRepository.cs
@@ -0,0 +1,23 @@
+﻿using Disk.Db.Context;
+using Disk.Entity;
+
+namespace Disk.Repository
+{
+    public class DoctorPatientRepository(DiskContext context)
+    {
+        public async Task<int> AssignAppointment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AssignProcedure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddNote(Note note)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
