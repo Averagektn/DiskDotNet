e2f3813a64de5c4367be57b6fbeed6faa6433188
Glushachenko Nikita <rylonoboev2004@gmail.com>
2024-06-08 21:10:23 +0300

    Add patient view UI implemented

--- Disk/ViewModel/AddPatientViewModel.cs
+++ Disk/ViewModel/AddPatientViewModel.cs
@@ -1,9 +1,21 @@
-﻿using Disk.ViewModel.Common.ViewModels;
+﻿using Disk.Entities;
+using Disk.Stores;
+using Disk.ViewModel.Common.Commands.Sync;
+using Disk.ViewModel.Common.ViewModels;
+using System.Windows.Input;
 
 namespace Disk.ViewModel
 {
-    public class AddPatientViewModel : PopupViewModel
+    public class AddPatientViewModel(ModalNavigationStore modalNavigationStore) : PopupViewModel
     {
+        private Patient _patient = new();
+        public Patient Patient { get => _patient; set => SetProperty(ref _patient, value); }
 
+        public ICommand AddPatientCommand => new Command(AddPatient);
+
+        private void AddPatient(object? obj)
+        {
+
+        }
     }
 }
