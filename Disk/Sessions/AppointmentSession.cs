37895607a376a17a93cfc08f6b254efe81f86381
Glushachenko Nikita <rylonoboev2004@gmail.com>
2024-06-09 21:11:38 +0300

    Current session

--- Disk/Sessions/AppointmentSession.cs
+++ Disk/Sessions/AppointmentSession.cs
@@ -6,5 +6,6 @@ namespace Disk.Sessions
     {
         public static Patient Patient { get; set; } = new();
         public static Appointment Appointment { get; set; } = new();
+        public static Session CurrentSession { get; set; } = new();
     }
 }
