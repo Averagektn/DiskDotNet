8416119eb1b976172ed7dc0209fbff9dce51bbb3
Glushachenko Nikita <rylonoboev2004@gmail.com>
2024-06-12 20:45:51 +0300

    First letter capitalization for UI

--- Disk/Entities/Doctor.cs
+++ Disk/Entities/Doctor.cs
@@ -1,14 +1,19 @@
-﻿namespace Disk.Entities;
+﻿using Disk.Extensions;
+
+namespace Disk.Entities;
 
 public partial class Doctor
 {
     public long Id { get; set; }
 
-    public string Name { get; set; } = null!;
+    private string _name = null!;
+    public string Name { get => _name; set => _name = value.CapitalizeFirstLetter(); }
 
-    public string Surname { get; set; } = null!;
+    private string _surname = null!;
+    public string Surname { get => _surname; set => _surname = value.CapitalizeFirstLetter(); }
 
-    public string? Patronymic { get; set; }
+    private string? _patronymic;
+    public string? Patronymic { get => _patronymic; set => _patronymic = value?.CapitalizeFirstLetter(); }
 
     public string Password { get; set; } = null!;
 
---
+++ Disk/Extensions/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Disk.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string CapitalizeFirstLetter(this string str) => 
+            string.IsNullOrEmpty(str) ? str : char.ToUpper(str[0]) + str[1..];
+    }
+}
