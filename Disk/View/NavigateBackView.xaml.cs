f77812aa55fcc94b045cd8428374b35c93d3094b
Glushachenko Nikita <rylonoboev2004@gmail.com>
2024-06-22 20:27:13 +0300

    UI fixes

--- Disk/View/NavigateBackView.xaml.cs
+++ Disk/View/NavigateBackView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Disk.View
 {
@@ -23,6 +11,20 @@ namespace Disk.View
         public NavigateBackView()
         {
             InitializeComponent();
+
+            SizeChanged += OnSizeChanged;
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            const int iniFontSize = 15;
+            const int iniHeight = 400;
+            const int iniWidth = 800;
+
+            double heightScale = e.NewSize.Height / iniHeight;
+            double widthScale = e.NewSize.Width / iniWidth;
+            Menu.FontSize = iniFontSize * double.Min(widthScale, heightScale);
+            Menu.Height = Menu.FontSize + 10;
         }
     }
 }
