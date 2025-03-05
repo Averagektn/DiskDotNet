94c9fe73a22ae71533ebd53d0dff0fa171a5e261
Averagektn <90308369+Averagektn@users.noreply.github.com>
2025-03-05 20:37:52 +0300

    TargetPucture created

---
+++ Disk/Visual/Impl/TargetPicture.cs
@@ -0,0 +1,11 @@
+﻿using Disk.Data.Impl;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Disk.Visual.Impl;
+
+public class TargetPicture(Point2D<int> center, int radius, int speed, Brush color, Canvas parent, Size iniSize) :
+    User(center, radius, speed, color, parent, iniSize)
+{
+}
