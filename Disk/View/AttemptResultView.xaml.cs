c4ad306c5d4d09ee33b6cff9482445ebf97d2503
Averagektn <90308369+Averagektn@users.noreply.github.com>
2025-04-29 20:03:57 +0300

    Refactor AttemptResultView layout and add animations
    Restructured the XAML layout of `AttemptResultView` to use a new `Grid` named `PaintGrid`, consolidating multiple `Canvas` elements for improved organization. Added a sidebar with a `ScrollViewer` containing various controls to enhance user interaction.
    
    Updated `AttemptResultView.xaml.cs` with new fields and methods to manage UI element visibility, reintroduced the `OnRender` method for rendering logic, and implemented methods for showing and hiding elements.
    
    Added animation functionality to the sidebar for a smoother user experience during mouse interactions. Updated `using` directives to include `System.Windows.Media.Animation` for animation features.

--- Disk/View/AttemptResultView.xaml.cs
+++ Disk/View/AttemptResultView.xaml.cs
@@ -8,6 +8,7 @@ using System.Data;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Threading;
 using Settings = Disk.Properties.Config.Config;
 
@@ -26,11 +27,6 @@ public partial class AttemptResultView : UserControl
     private readonly Size _iniScreenSize = new(_settings.IniScreenWidth, _settings.IniScreenHeight);
 
     private static readonly Settings _settings = Settings.Default;
-
-    private int _selectedIndex;
-    private IEnumerator<(bool IsNewTarget, Point2D<float> Point)>? _enumerator;
-    private Point2D<int>? _replyCenter;
-    private DispatcherTimer? _coordTimer;
 
     private AttemptResultViewModel? ViewModel => DataContext as AttemptResultViewModel;
     private Size PaintPanelSize => PaintArea.RenderSize;
@@ -57,34 +53,12 @@ public partial class AttemptResultView : UserControl
         SizeChanged += (_, _) => Converter?.Scale(PaintPanelSize);
 
         Loaded += SelectionChanged;
+        Loaded += (_, _) => SidebarTransform.X = Sidebar.ActualWidth + 100;
 
         Unloaded += StopReply;
         CompositionTarget.Rendering += OnRender;
     }
 
-    private void OnRender(object? sender, EventArgs e)
-    {
-        if (!IsReply || ViewModel is null || _user is null || _target is null)
-        {
-            return;
-        }
-
-        // _enumerator is always not null. See ReplyClick method
-        if (_replyCenter is not null && IsReply)
-        {
-            _user.Move(_replyCenter);
-
-            if (_enumerator!.Current.IsNewTarget && ++_selectedIndex < ViewModel.TargetCenters.Count)
-            {
-                _target.Move(ViewModel.Converter.ToWndCoord(ViewModel.TargetCenters[_selectedIndex]));
-                ViewModel.SelectedIndex = _selectedIndex;
-            }
-        }
-        else
-        {
-            IsReply = false;
-        }
-    }
 
     private void SelectionChanged(object sender, RoutedEventArgs e)
     {
@@ -148,6 +122,7 @@ public partial class AttemptResultView : UserControl
         ellipseList.AddRange([convexHull, boundingEllipse]);
     }
 
+    #region Ellipse and ConvexHull creation
     private IStaticFigure CreateConvexHull(List<Point2D<float>> points, Brush borderColor, Brush fillColor)
     {
         ConvexHull ch;
@@ -183,6 +158,39 @@ public partial class AttemptResultView : UserControl
 
         return ellipse;
     }
+    #endregion
+
+    #region Reply
+
+    private int _selectedIndex;
+    private IEnumerator<(bool IsNewTarget, Point2D<float> Point)>? _enumerator;
+    private Point2D<int>? _replyCenter;
+    private DispatcherTimer? _coordTimer;
+
+    private void OnRender(object? sender, EventArgs e)
+    {
+        if (!IsReply || ViewModel is null || _user is null || _target is null)
+        {
+            return;
+        }
+
+        // _enumerator is always not null. See ReplyClick method
+        if (_replyCenter is not null && IsReply)
+        {
+            _user.Move(_replyCenter);
+
+            if (_enumerator!.Current.IsNewTarget && ++_selectedIndex < ViewModel.TargetCenters.Count)
+            {
+                _target.Move(ViewModel.Converter.ToWndCoord(ViewModel.TargetCenters[_selectedIndex]));
+                ViewModel.SelectedIndex = _selectedIndex;
+            }
+        }
+        else
+        {
+            IsReply = false;
+        }
+    }
+
 
     private void StopReply(object sender, RoutedEventArgs e)
     {
@@ -224,8 +232,12 @@ public partial class AttemptResultView : UserControl
         IsReply = true;
         _coordTimer.Start();
     }
+    #endregion
 
+    #region Showing and hiding elements
+    #region Path to target ellipse
     private bool _isPathToTargetEllipseVisible = false;
+
     public void ShowPathToTargetEllipse(object sender, RoutedEventArgs e)
     {
         if (!_isPathToTargetEllipseVisible)
@@ -238,6 +250,7 @@ public partial class AttemptResultView : UserControl
             _isPathToTargetEllipseVisible = true;
         }
     }
+
     public void HidePathToTargetEllipse(object sender, RoutedEventArgs e)
     {
         if (_isPathToTargetEllipseVisible)
@@ -246,8 +259,11 @@ public partial class AttemptResultView : UserControl
             _isPathToTargetEllipseVisible = false;
         }
     }
+    #endregion
 
+    #region Path in target ellipse
     private bool _isPathInTargetEllipseVisible = false;
+
     public void ShowPathInTargetEllipse(object sender, RoutedEventArgs e)
     {
         if (!_isPathInTargetEllipseVisible)
@@ -268,8 +284,11 @@ public partial class AttemptResultView : UserControl
             _isPathInTargetEllipseVisible = false;
         }
     }
+    #endregion
 
+    #region Full path ellipse
     private bool _isFullPathEllipseVisible = false;
+
     public void ShowFullPathEllipse(object sender, RoutedEventArgs e)
     {
         if (!_isFullPathEllipseVisible)
@@ -290,8 +309,11 @@ public partial class AttemptResultView : UserControl
             _isFullPathEllipseVisible = false;
         }
     }
+    #endregion
 
+    #region Cursor
     private bool _isCursorVisible = true;
+
     private void ShowCursor(object sender, RoutedEventArgs e)
     {
         if (!_isCursorVisible)
@@ -300,6 +322,7 @@ public partial class AttemptResultView : UserControl
             _user?.Draw();
         }
     }
+
     private void HideCursor(object sender, RoutedEventArgs e)
     {
         if (_isCursorVisible)
@@ -308,8 +331,11 @@ public partial class AttemptResultView : UserControl
             _user?.Remove();
         }
     }
+    #endregion
 
+    #region Target
     private bool _isTargetVisible = true;
+
     private void ShowTarget(object sender, RoutedEventArgs e)
     {
         if (!_isTargetVisible)
@@ -318,6 +344,7 @@ public partial class AttemptResultView : UserControl
             _target?.Draw();
         }
     }
+
     private void HideTarget(object sender, RoutedEventArgs e)
     {
         if (_isTargetVisible)
@@ -326,4 +353,38 @@ public partial class AttemptResultView : UserControl
             _target?.Remove();
         }
     }
+    #endregion
+    #endregion
+
+    #region Sidebar animations
+    private Brush _detectionAreaColor = Brushes.Transparent;
+    private void MouseDetectionArea_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        _detectionAreaColor = MouseDetectionArea.Background;
+        MouseDetectionArea.Background = Brushes.Transparent;
+
+        var animation = new DoubleAnimation
+        {
+            To = 0,
+            Duration = TimeSpan.FromMilliseconds(300)
+        };
+        SidebarTransform.BeginAnimation(TranslateTransform.XProperty, animation);
+
+        DimOverlay.Visibility = Visibility.Visible;
+    }
+
+    private void Sidebar_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        MouseDetectionArea.Background = _detectionAreaColor;
+
+        var animation = new DoubleAnimation
+        {
+            To = Sidebar.ActualWidth + 100,
+            Duration = TimeSpan.FromMilliseconds(300)
+        };
+        SidebarTransform.BeginAnimation(TranslateTransform.XProperty, animation);
+
+        DimOverlay.Visibility = Visibility.Hidden;
+    }
+    #endregion
 }
